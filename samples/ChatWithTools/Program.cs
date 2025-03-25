d23b8f446ceb2230ae92030ce714315cc86f786b
lark <lilinghui@gmail.com>
2025-03-25 11:14:44 +0800

    use github.com/marketplace/models

--- samples/ChatWithTools/Program.cs
+++ samples/ChatWithTools/Program.cs
@@ -1,4 +1,4 @@
-﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Client;
 using ModelContextProtocol.Protocol.Transport;
 using Microsoft.Extensions.AI;
 using OpenAI;
@@ -28,9 +28,20 @@ Console.WriteLine();
 
 // Create an IChatClient. (This shows using OpenAIClient, but it could be any other IChatClient implementation.)
 // Provide your own OPENAI_API_KEY via an environment variable.
+// using IChatClient chatClient =
+//     new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY")).AsChatClient("gpt-4o-mini")
+//     .AsBuilder().UseFunctionInvocation().Build();
+
+// Use https://github.com/marketplace/models/azure-openai/gpt-4o/playground
+var credential = new System.ClientModel.ApiKeyCredential(System.Environment.GetEnvironmentVariable("GITHUB_TOKEN")!);
+var openAIOptions = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.inference.ai.azure.com")
+};
 using IChatClient chatClient =
-    new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY")).AsChatClient("gpt-4o-mini")
+    new OpenAI.Chat.ChatClient("gpt-4o-mini", credential, openAIOptions).AsChatClient()
     .AsBuilder().UseFunctionInvocation().Build();
+
 
 // Have a conversation, making all tools available to the LLM.
 List<ChatMessage> messages = [];
